49b1f0daf0e65b91a01d07e967b06e0ed7075ae9
LyhourChhen <ukamilove66@gmail.com>
2020-04-06 12:48:23 +0700

    update

--- src/prisma.js
+++ src/prisma.js
@@ -74,7 +74,7 @@ const prisma = new Prisma({
 
 const createPostForUser = async (authorId, data) => {
     // check if user exist or not
-    const userExist = prisma.exists.User({ id: authorId })
+    const userExist = await prisma.exists.User({ id: authorId })
     // create post
     if (userExist === false) {
         throw new Error("user don't exist, pls check your user again !")
@@ -104,15 +104,20 @@ const createPostForUser = async (authorId, data) => {
 
     return post.author
 }
-createPostForUser('ck8l5p9ii006i07044xqxxhmv', {
-    title: 'These books that you should be read in your life',
-    body: 'The art of war',
-    published: true,
-})
-    .then((user) => console.log(JSON.stringify(user, null, 2)))
-    .catch((err) => console.log(err.message))
+// createPostForUser('ck8l5p9ii006i07044xqxxhmv', {
+//     title: 'These books that you should be read in your life',
+//     body: 'The art of war',
+//     published: true,
+// })
+//     .then((user) => console.log(JSON.stringify(user, null, 2)))
+//     .catch((error) => console.log(colors.red(error.message)))
 
 const updatePostForUser = async (postId, data) => {
+    // check the post exist or not
+    const existPost = await prisma.exists.Post({ id: postId })
+    if (!existPost) {
+        throw new Error('post not exist')
+    }
     const post = await prisma.mutation.updatePost(
         {
             where: {
@@ -122,30 +127,33 @@ const updatePostForUser = async (postId, data) => {
                 ...data,
             },
         },
-        '{author {id}}',
+        '{author {id name email posts {id title published}}}',
     )
-    const user = await prisma.query.user(
-        {
-            where: {
-                id: post.author.id,
-            },
-        },
-        '{id name email posts {id title published}}',
-    )
-    return user
+    // const user = await prisma.query.user(
+    //     {
+    //         where: {
+    //             id: post.author.id,
+    //         },
+    //     },
+    //     '{id name email posts {id title published}}',
+    // )
+    return post.author
 }
 
-// updatePostForUser('ck8lgen6700vw0704hrpq54z0', {
+// updatePostForUser('ck8o0zymb00cn0704x23lnbr5', {
 //     published: false,
-// }).then((user) => console.log(JSON.stringify(user, null, 2)))
+// })
+//     .then((user) => console.log(JSON.stringify(user, null, 2)))
+//     .catch((error) => console.log(colors.red(error.message)))
 
 // Prisma Utility Existence
 // Note : use <Name> from <Schema Name>
-prisma.exists
-    .Comment({
-        id: 'ck8l7xg3200ms07040sta110u',
-        author: {
-            id: 'ck8l7soex00jq0704p2an94z2',
-        },
-    })
-    .then((exist) => console.log('Does comment exist ? :', exist))
+
+// prisma.exists
+//     .Comment({
+//         id: 'ck8l7xg3200ms07040sta110u',
+//         author: {
+//             id: 'ck8l7soex00jq0704p2an94z2',
+//         },
+//     })
+//     .then((exist) => console.log('Does comment exist ? :', exist))
