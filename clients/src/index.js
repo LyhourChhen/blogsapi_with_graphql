af59ce910b085b5fa82e50d2024c67173ee6c55d
LyhourChhen <ukamilove66@gmail.com>
2020-04-10 11:39:34 +0700

    connect client with server with apollo

--- clients/src/index.js
+++ clients/src/index.js
@@ -14,10 +14,48 @@ const getUsers = gql`
     }
 `
 
+const getPosts = gql`
+    query {
+        posts {
+            id
+            title
+        }
+    }
+`
+
 client
     .query({
         query: getUsers,
     })
     .then((res) => {
         console.log('getting data from graphQL', res.data.users)
+
+        let html = ''
+        res.data.users.forEach((user) => {
+            html += `
+                <div>
+                    <h3>${user.name}</h3>
+                </div
+            `
+        })
+        document.getElementById('users').innerHTML = html
+    })
+
+client
+    .query({
+        query: getPosts,
+    })
+    .then((res) => {
+        console.log('getting response from user with graphQL', res.data.posts)
+
+        let html = ''
+        res.data.posts.forEach((post) => {
+            html += `
+                <div>
+                    <p style={{fontWeight: "bold}}>${post.id}</p>
+                    <h3>${post.title}</h3>
+                </div>
+            `
+        })
+        document.getElementById('posts').innerHTML = html
     })
