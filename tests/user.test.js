8a0288c43e3fe01a6d9736d32289685f60ebd671
LyhourChhen <ukamilove66@gmail.com>
2020-04-10 13:17:46 +0700

    update test suit with query with jest

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,30 +1,61 @@
-import { getFirstName, validatePassword } from '../src/utils/user'
-// first test
-test('should render out', () => {
-    console.log('haha')
+import 'cross-fetch/polyfill'
+import ApolloBoost, { gql } from 'apollo-boost'
+
+const client = new ApolloBoost({
+    uri: 'http://localhost:4000',
 })
 
-// render the first name only
-
-test('should get only firstName', () => {
-    const firstName = getFirstName('Lyhour Chhen')
-
-    // manually approach
-    // if (firstName !== 'Lyhour') {
-    //     throw new Error('Expected name Lyhour')
-    // }
-
-    // with expect method from jest
-    expect(firstName).toBe('Lyhour')
+test('should create a new user', async () => {
+    const createUser = gql`
+        mutation {
+            createUser(
+                data: {
+                    name: "LyhourChhen"
+                    email: "chhenlyhour1sssss@gmail.com"
+                    password: "Cambodia@123"
+                }
+            ) {
+                token
+                user {
+                    id
+                    name
+                    email
+                }
+            }
+        }
+    `
+    const response = await client.mutate({
+        mutation: createUser,
+    }).then
 })
 
-test('should reject password when length less than 8 ', () => {
-    const valid = validatePassword('9se38')
+// import { getFirstName, validatePassword } from '../src/utils/user'
+// // first test
+// test('should render out', () => {
+//     console.log('haha')
+// })
 
-    expect(valid).toBe(false)
-})
+// // render the first name only
 
-test('should reject that password is contain the word => password', () => {
-    const valid = validatePassword('passwsord')
-    expect(valid).toBe(true)
-})
+// test('should get only firstName', () => {
+//     const firstName = getFirstName('Lyhour Chhen')
+
+//     // manually approach
+//     // if (firstName !== 'Lyhour') {
+//     //     throw new Error('Expected name Lyhour')
+//     // }
+
+//     // with expect method from jest
+//     expect(firstName).toBe('Lyhour')
+// })
+
+// test('should reject password when length less than 8 ', () => {
+//     const valid = validatePassword('9se38')
+
+//     expect(valid).toBe(false)
+// })
+
+// test('should reject that password is contain the word => password', () => {
+//     const valid = validatePassword('passwsord')
+//     expect(valid).toBe(true)
+// })
