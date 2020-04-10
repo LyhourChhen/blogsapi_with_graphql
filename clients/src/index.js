0974fe28aba152301687359fe29966920468ff13
LyhourChhen <ukamilove66@gmail.com>
2020-04-10 11:10:56 +0700

    connect client with server with apollo

--- clients/src/index.js
+++ clients/src/index.js
@@ -1,1 +1,23 @@
-console.log('helo')
+import ApolloBoost, { gql } from 'apollo-boost'
+
+const client = new ApolloBoost({
+    uri: 'http://localhost:4000',
+})
+
+const getUsers = gql`
+    query {
+        users {
+            name
+            id
+            email
+        }
+    }
+`
+
+client
+    .query({
+        query: getUsers,
+    })
+    .then((res) => {
+        console.log('getting data from graphQL', res.data.users)
+    })
